ff506342dfbd5f9608e698566bd0f10f50811ad4
mando_christ <mandochrist13@gmail.com>
2024-10-06 14:21:30 +0100

    hero section

--- src/app/_components/hero.js
+++ src/app/_components/hero.js
@@ -8,13 +8,13 @@ export default function HeroSection() {
   const [isVisible] = useState(true);
   return (
     <header
-      className="bg-cover bg-center h-svh"
-      style={{ backgroundImage: "url(/assets/Section.png)" }}
+      className="bg-cover bg-center h-screen"
+      style={{ backgroundImage: "url(/assets/terre4.gif)" }}
     >
-      <div className="container h-full mx-auto">
+      <div className="opacity-70 bg-black bg-blend-overlay w-full h-full">
         <div className="flex h-full flex-col-reverse md:flex-row items-center justify-center md:justify-around gap-4 px-5">
           <AnimatePresence>
-            <div className="">
+            <div className="hidden">
               <Image
                 src="/assets/globe.gif"
                 width={200}
@@ -25,15 +25,16 @@ export default function HeroSection() {
             </div>
             {isVisible && (
               <motion.div
-                initial={{ opacity: 0, scale: 0 }}
-                whileInView={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.6 }}
-                className="flex flex-col gap-4 w-full md:w-1/3 max-w-[500px]"
-              >
-                <h1 className="text-[23px] md:text-4xl text-center font-bold text-white">
+              initial={{ opacity: 0, y: 30 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.10, ease: "easeOut" }}
+              className="flex flex-col gap-4 w-full max-w-[600px]"
+            >
+              <h1 className="text-[33px] md:text-[45px] text-center font-bold text-white">
                 Un Avertissement pour notre Planète : L&apos;Histoire de Nos Choix
-                </h1>
-              </motion.div>
+              </h1>
+            </motion.div>
+            
             )}
           </AnimatePresence>
         </div>
