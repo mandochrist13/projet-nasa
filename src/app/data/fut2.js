9491434de5305d807fae9edaa96d04b31561b2e4
mando_christ <mandochrist13@gmail.com>
2024-10-06 07:11:51 +0100

    elements finaux

--- src/app/data/fut2.js
+++ src/app/data/fut2.js
@@ -4,7 +4,7 @@ const futurBad = [
         titre: "Rôle Essentiel des Forêts",
         photo: "/assets/terre_fu2.jpg",
         description: "Les forêts, en jouant un rôle essentiel dans le cycle du carbone, contribuent à atténuer les effets du changement climatique à l'échelle mondiale.", 
-        href: "https://skybox.blockadelabs.com/e/cd57453af6c832f864d1868b7fbcbf24",
+        href: "https://skybox.blockadelabs.com/e/bca2ec74ff8aa518e3ed7df6825f5523",
         explication:"Les forêts, souvent appelées les poumons de la Terre, jouent un rôle crucial dans la régulation du climat en absorbant le dioxyde de carbone (CO₂) de l'atmosphère. Elles constituent environ 31 % de la surface terrestre et abritent une biodiversité essentielle pour l'équilibre des écosystèmes ainsi que des ressources vitales pour les communautés humaines. Cependant, la déforestation, alimentée par l'agriculture intensive et l'urbanisation, entraîne une perte alarmante de ces écosystèmes. Près de 10 millions d'hectares de forêt disparaissent chaque année, aggravant le changement climatique. Pour préserver les forêts, il est impératif d'adopter des pratiques de conservation telles que la reforestation et la gestion durable des ressources forestières. Sensibiliser les communautés à leur importance et encourager des initiatives locales, comme des programmes de reboisement, sont des actions concrètes que chacun peut entreprendre. Le soutien à des politiques environnementales robustes est également essentiel. Les gouvernements et les citoyens doivent collaborer pour protéger les forêts et promouvoir des pratiques durables. En somme, la conservation des forêts est essentielle pour lutter contre le changement climatique et assurer un avenir durable. Protéger nos forêts, c'est investir dans la santé de notre planète pour les générations futures."
     },
     {
@@ -12,15 +12,15 @@ const futurBad = [
         titre: "Les Océans au Service du Climat",
         photo: "/assets/ocean_fu2.jpg",
         description: "En contrôlant le réchauffement climatique, nous ralentissons l'élévation du niveau de la mer, ce qui aide à protéger les écosystèmes côtiers.",
-        href: "",
-        explication:""
+        href: "https://skybox.blockadelabs.com/e/63034f8cc3c8180c67e4a036ada3841c",
+        explication:"Le réchauffement climatique cause une élévation du niveau de la mer, qui a augmenté d'environ 3,3 mm par an depuis 1993, selon le Groupe d'experts intergouvernemental sur l'évolution du climat GIEC. Cette montée des eaux met en péril les écosystèmes côtiers, tels que les mangroves et les récifs coralliens, qui sont essentiels pour protéger les côtes contre l'érosion et les tempêtes. Les mangroves, par exemple, peuvent atténuer la force des vagues de 50 à 70 %. Cependant, l'acidification des océans, provoquée par l'absorption croissante de CO₂, a augmenté de 30 % depuis la Révolution industrielle, menaçant ainsi des espèces marines vitales. Actuellement, 75 % des récifs coralliens sont en danger, avec un risque de disparition d'ici 2050 si les émissions ne sont pas réduites. Limiter le réchauffement à 1,5 °C, comme stipulé dans l'Accord de Paris, pourrait diminuer l'élévation du niveau de la mer de 30 % d'ici la fin du siècle. Protéger nos écosystèmes côtiers est donc crucial pour la résilience des communautés et la biodiversité marine. Il est impératif d'agir dès maintenant pour assurer un avenir durable."
     },
     {
         id:12,
         titre: "Raffraîchissement Urbain",
         photo: "/assets/DEMO_FU2.jpg",
         description: "Les arbres apportent également d'autres bénéfices, comme la diminution des effets des îlots de chaleur urbains.",
-        href: "",
+        href: "https://skybox.blockadelabs.com/e/a61d5cc17596a428099aa02cc15c22c2",
         explication:"Familiarisez-vous avec votre communauté en identifiant les problèmes climatiques qui menacent vos quartiers, les populations les plus vulnérables et les moyens d'apporter votre aide. Utilisez l'outil EJSCREEN de l'EPA pour repérer les zones susceptibles de connaître des charges environnementales plus élevées et des communautés vulnérables. Plantez des arbres là où vous le pouvez, mais n'arrêtez pas vos efforts là. Les plantations d'arbres, les jardins communautaires, les toits verts et d'autres initiatives peuvent contribuer à atténuer les îlots de chaleur urbains. Les communautés défavorisées sont particulièrement exposées aux effets de ces îlots de chaleur, surtout pendant l'été."
     }]
 
