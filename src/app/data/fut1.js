6b18accbd4f06af94d90dc5e476fa893acfc37b1
mando_christ <mandochrist13@gmail.com>
2024-10-06 06:35:10 +0100

    ajout des images panaramiques

--- src/app/data/fut1.js
+++ src/app/data/fut1.js
@@ -4,24 +4,24 @@ const futurGood = [
         titre: "Changement Climatique",
         photo: "/assets/terre_fu1.jpg",
         description: "Le changement climatique impactera divers facteurs nuisibles à la santé des forêts, tels que les épidémies d'insectes, les espèces envahissantes, les incendies de forêt et les tempêtes.", 
-        href: "https://skybox.blockadelabs.com/e/cd57453af6c832f864d1868b7fbcbf24",
-        explication:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum nobis, rerum esse itaque temporibus dolore, impedit maxime obcaecati voluptates incidunt neque deserunt eaque quasi doloremque aliquid assumenda sunt ex? Voluptatum. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum ex molestias corporis numquam tempora nesciunt. Animi sit corrupti provident, labore suscipit quos voluptatum aliquam commodi? Cupiditate quaerat aliquid assumenda consequuntur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi autem alias eum modi distinctio nihil, assumenda dolore. Ut vero, aliquam, error deserunt tempore magnam sapiente quibusdam voluptates, non molestiae eligendi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum nobis, rerum esse itaque temporibus dolore, impedit maxime obcaecati voluptates incidunt neque deserunt eaque quasi doloremque aliquid assumenda sunt ex? Voluptatum. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum ex molestias corporis numquam tempora nesciunt. Animi sit corrupti provident, labore suscipit quos voluptatum aliquam commodi? Cupiditate quaerat aliquid assumenda consequuntur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi autem alias eum modi distinctio nihil, assumenda dolore. Ut vero, aliquam, error deserunt tempore magnam sapiente quibusdam voluptates, non molestiae eligendi?"
+        href:"https://skybox.blockadelabs.com/e/08ea1ab8319b75863aac5969edecb516",
+        explication:"Les sécheresses ont des impacts négatifs sur l'agriculture, l'approvisionnement en eau, la production d'énergie, la santé humaine et d'autres domaines de la société. Les conséquences varient selon le type de sécheresse, son emplacement, son intensité et sa durée. Par exemple, en agriculture, les sécheresses peuvent ralentir la croissance des plantes ou entraîner des pertes de récoltes importantes. En ce qui concerne l'eau, elles peuvent provoquer une diminution des niveaux des réservoirs, l'assèchement des cours d'eau et des pénuries sévères. Les sécheresses prolongées constituent une menace particulière pour les populations autochtones, qui dépendent économiquement et culturellement de la terre et de l'eau. De plus, ces conditions climatiques peuvent mettre en danger des plantes et des animaux médicinaux, diminuer la qualité de l'eau et rendre les communautés tribales plus vulnérables aux maladies d'origine hydrique."
     },
     {
         id:8,
         titre: "L'Enjeu de la Montée des Eaux",
         photo: "/assets/ocean_fu1.jpg",
         description: "Le changement climatique entraîne des événements météorologiques extrêmes, notamment des pluies torrentielles qui saturent le sol et causent des inondations.",
-        href: "",
-        explication:""
+        href: "https://skybox.blockadelabs.com/e/935152082ba5aad000bc027033e94bef",
+        explication:"Le changement climatique entraîne une augmentation des événements météorologiques extrêmes, en particulier des pluies torrentielles. Selon la National Oceanic and Atmospheric Administration (NOAA), la fréquence de ces pluies a augmenté de 27 % aux États-Unis depuis 1950, avec des tendances similaires observées ailleurs. Ces pluies intenses saturent rapidement le sol, rendant son absorption impossible. En 2021, des tempêtes dans des régions comme le New Jersey et le Mississippi ont déversé plus de 10 pouces de pluie en quelques heures, provoquant d'importantes inondations. En 2022, des inondations soudaines en Europe centrale ont causé des pertes matérielles dépassant 1,4 milliard d'euros. Les conséquences des inondations sont graves, entraînant des dommages matériels et affectant la sécurité des communautés. Depuis 1979, plus de 14 000 Américains ont perdu la vie en raison de conditions liées à la chaleur, souvent exacerbées par des inondations."
     },
     {
         id:9,
         titre: "Pollution de l'Air",
         photo: "/assets/demo_fu1.jpg",
         description: "La pollution de l'air liée aux activités industrielles est associée à des problèmes de santé, comme des maladies respiratoires, cardiovasculaires et des cancers.",
-        href: "",
-        explication:""
+        href: "https://skybox.blockadelabs.com/e/7e66c80ef886256b8803d4390f1a0582",
+        explication:"Les changements climatiques peuvent avoir des impacts significatifs sur la santé publique, l'agriculture, l'approvisionnement en eau, la production et l'utilisation de l'énergie, ainsi que sur l'utilisation des terres et les loisirs. Ils modifient les conditions météorologiques habituelles, créant ainsi des menaces pour la santé et le bien-être des populations. Depuis 1979, plus de 14 000 Américains sont décédés directement en raison de la chaleur, et entre 1992 et 2022, 986 travailleurs dans divers secteurs ont perdu la vie à cause de l'exposition à la chaleur. Le taux de mortalité est encore plus élevé lorsque l'on prend en compte les décès liés à la chaleur en tant que facteur contributif, notamment en cas d'interaction avec des maladies cardiovasculaires. Les personnes âgées de 65 ans et plus sont particulièrement vulnérables, étant plusieurs fois plus susceptibles de mourir de maladies cardiovasculaires liées à la chaleur que le reste de la population."
     }]
 
     export {futurGood};
--- src/app/data/mid.js
+++ src/app/data/mid.js
@@ -4,7 +4,7 @@ const midsections = [
         titre: "Climat et Nature Avant l'Ère Industrielle",
         photo: "/assets/DEMO_PA.jpg",
         description: "Avant la Révolution industrielle, qui a débuté vers la fin du XVIIIe siècle, le climat et l’environnement terrestre étaient marqués par un équilibre naturel", 
-        href: "https://skybox.blockadelabs.com/e/cd57453af6c832f864d1868b7fbcbf24",
+        href: "https://skybox.blockadelabs.com/e/bc81f5aae749dc0ef5e2aff6fd49157f",
         explication:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum nobis, rerum esse itaque temporibus dolore, impedit maxime obcaecati voluptates incidunt neque deserunt eaque quasi doloremque aliquid assumenda sunt ex? Voluptatum. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum ex molestias corporis numquam tempora nesciunt. Animi sit corrupti provident, labore suscipit quos voluptatum aliquam commodi? Cupiditate quaerat aliquid assumenda consequuntur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi autem alias eum modi distinctio nihil, assumenda dolore. Ut vero, aliquam, error deserunt tempore magnam sapiente quibusdam voluptates, non molestiae eligendi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum nobis, rerum esse itaque temporibus dolore, impedit maxime obcaecati voluptates incidunt neque deserunt eaque quasi doloremque aliquid assumenda sunt ex? Voluptatum. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum ex molestias corporis numquam tempora nesciunt. Animi sit corrupti provident, labore suscipit quos voluptatum aliquam commodi? Cupiditate quaerat aliquid assumenda consequuntur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi autem alias eum modi distinctio nihil, assumenda dolore. Ut vero, aliquam, error deserunt tempore magnam sapiente quibusdam voluptates, non molestiae eligendi?"
     },
     {
@@ -12,7 +12,7 @@ const midsections = [
         titre: "Les Océans comme Stabilisateurs Climatiques",
         photo: "/assets/océan_pa.jpg",
         description: "Les océans étaient en équilibre, jouant un rôle de régulateurs naturels du climat en absorbant le CO₂ de l'atmosphère et en stabilisant les températures.",
-        href: "",
+        href: "https://skybox.blockadelabs.com/e/3ddbc43e6c5cf7e219157713c8da5d5c",
         explication:""
     },
     {
@@ -20,7 +20,7 @@ const midsections = [
         titre: "La Nature au Service de la Qualité de l'Air",
         photo: "/assets/terre_pa.jpg",
         description: "L'air était plus pur, avec des niveaux de pollution bien inférieurs à ceux de l'ère industrielle, et les écosystèmes régulaient naturellement les émissions de gaz à effet de serre.",
-        href: "",
+        href: "https://skybox.blockadelabs.com/e/145b908e2529b22978c14cab16f583a5",
         explication:""
     }]
 
